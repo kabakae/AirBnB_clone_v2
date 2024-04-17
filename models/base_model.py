efee29fa2004780acea3ab3095c151121a4fac28
kabakae <kabakaevans@gmail.com>
2024-04-17 10:54:18 -0700

    000

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,13 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+from engine import storage
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
