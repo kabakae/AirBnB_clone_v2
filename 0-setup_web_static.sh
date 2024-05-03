0a34907f01a392252095ebb105239af7f401cafa
kabakae <kabakaevans@gmail.com>
2024-05-03 15:29:36 -0700

    000

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 # script that sets up web servers for deployment
+
+# Update package index
 sudo apt-get update
+
+# Install Nginx
 sudo apt-get -y install nginx
+
+# Allow Nginx HTTP traffic
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/release/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-   <head>
-   </head>
-   <body>
-     Holberton School
-   </body>
+# Create necessary directories
+sudo mkdir -p /data/web_static/{releases/test,shared}
 
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create index.html file
+sudo tee /data/web_static/releases/test/index.html >/dev/null <<EOF
+<html>
+<head>
+</head>
+<body>
+  Holberton School
+</body>
+</html>
+EOF
+
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Change ownership
 sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+# Restart Nginx
 sudo service nginx restart
