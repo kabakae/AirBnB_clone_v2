32ee4cf02eb4296dd3b76ab3641ac3785e471637
kabakae <evanskabaka@gmail.com>
2024-05-05 01:18:24 -0700

    0000

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,25 +23,28 @@ def do_deploy(archive_path):
         # Upload the archive to /tmp/ directory of the web servers
         put(archive_path, '/tmp/')
 
-        # Extract archive to /data/web_static/releases/<archive filename without extension>
+        # Extract archive to /data/web_static/releases/<archive filename >
         archive_filename = os.path.basename(archive_path)
         archive_name = os.path.splitext(archive_filename)[0]
         releases_path = '/data/web_static/releases/'
 
         run('mkdir -p {}{}'.format(releases_path, archive_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_filename, releases_path, archive_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_filename,
+            releases_path, archive_name))
 
         # Remove the uploaded archive
         run('rm /tmp/{}'.format(archive_filename))
 
         # Move files from extracted folder to the correct location
-        run('mv {}{}/web_static/* {}{}/'.format(releases_path, archive_name, releases_path, archive_name))
+        run('mv {}{}/web_static/* {}{}/'.format(releases_path, archive_name,
+            releases_path, archive_name))
 
         # Remove the old symbolic link
         run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link
-        run('ln -s {}{}/ /data/web_static/current'.format(releases_path, archive_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(releases_path,
+            archive_name))
 
         print("New version deployed!")
         return True
