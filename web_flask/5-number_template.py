2a19705cb3bda9f3dded97a92aee0fc4426a769f
root <evanskabaka@gmail.com>
2024-05-17 12:54:51 -0700

    000

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -15,20 +15,24 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Displays 'C ' followed by the value of the text variable"""
     return "C {}".format(escape(text).replace("_", " "))
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -36,15 +40,18 @@ def python_text(text="is cool"):
     """Displays 'Python ' followed by the value of the text variable"""
     return "Python {}".format(escape(text).replace("_", " "))
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """Displays 'n is a number' only if n is an integer"""
     return "{} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Displays an HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
