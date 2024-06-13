8c644188c4e30ab7f05a1c06858c0f3da9fb36e7
kabakae <evanskabaka@gmail.com>
2024-06-13 08:17:29 +0000

    00

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env bash
 """
 This script starts a Flask web application.
 It listens on 0.0.0.0, port 5000, and has two routes:
@@ -7,15 +7,15 @@ It listens on 0.0.0.0, port 5000, and has two routes:
 """
 
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask("__name__")
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return render_template("5-number.html")
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
