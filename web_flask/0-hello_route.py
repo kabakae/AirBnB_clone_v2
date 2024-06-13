4fb1e89b6a0dd5f9bf3c8c7fb6503cf41449c390
kabakae <evanskabaka@gmail.com>
2024-06-13 07:40:27 +0000

    00

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,15 +7,15 @@ It listens on 0.0.0.0, port 5000, and has two routes:
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
+def hello():
+    return render_template("5-number.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
